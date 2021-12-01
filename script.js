2f01f7769a73dcce83e1cabdc4e846c22dc5e499
Maxim <kejerino@yandex.ru>
2021-12-01 13:05:37 +0800

    create func

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let editButton = document.querySelector(".profile__edit-button");
 let page = document.querySelector(".page");
 let closeButton = document.querySelector(".popup__close-button")
+
 let popup = document.querySelector(".popup")
 
 let formElement = document.querySelector(".popup__form")
+
 let personInput = document.querySelector(".popup__person")
 let annotationInput = document.querySelector(".popup__annotation")
 
@@ -13,24 +15,24 @@ let personProfile = document.querySelector(".profile__person")
 editButton.addEventListener("click", function () {
     createPopup();
 });
+function createPopup() {
+    popup.classList.add("popup_opend");
+}
+
 
 closeButton.addEventListener("click", function () {
     closePopup();
 });
-
 function closePopup () {
-    return popup.style.display = "none"
+    popup.classList.remove("popup_opend");
 }
 
-function createPopup() {
-    return popup.style.display = "flex"
-}
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     personProfile.textContent = personInput.value;
     annotationProfile.textContent = annotationInput.value;
-    popup.style.display = "none"
+    closePopup();
 }
 
-formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', formSubmitHandler);
