c034174fe56977e3cb589a537d5350cb2ba629ad
Maxim <kejerino@yandex.ru>
2022-01-11 15:32:31 +0800

    project 5

--- scripts/script.js
+++ scripts/script.js
@@ -1,106 +1,90 @@
 const popup = document.querySelector('.popup')
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button')
-const formElement = document.querySelector('.popup__form')
+const popupForm = document.querySelector('.popup__form')
 const elementLike = document.querySelector('.element__like')
 const popupContainer = document.querySelector('.popup__container')
 const addButton = document.querySelector('.profile__add-button')
+const placeContainElements = document.querySelector('.elements')
+const placeElement = document.querySelectorAll('.element')
+
+const templateEditButton = document.querySelector('#edit-profile');
+const templateNewPlace = document.querySelector('#new-place');
 
 let personInput = document.querySelector('#person')
 let aboutMeInput = document.querySelector('#about-me')
 let aboutMeProfile = document.querySelector('.profile__about-me')
 let personProfile = document.querySelector('.profile__person')
-
-// Изначально хотел сделать шаблон template для каждой из форм, 
-// но это слишком усложняет код и проще будет написать через 2 функции  
-
-// Создание формы popup !--- Редактировать профиль ---!
-const createPopupEditButton = () => {
-    popup.classList.add('popup_opend');
-    popupContainer.querySelector('.popup__title').textContent = 'Редактировать профиль'; 
-    personInput.value = personProfile.textContent;
-    aboutMeInput.value= aboutMeProfile.textContent;
-    // Отправка формы для popup Редактировать профиль
-    const formSubmitEditProfile  = (evt) => {
-      evt.preventDefault(); 
-      personProfile.textContent = personInput.value;
-      aboutMeProfile.textContent = aboutMeInput.value;
-      closePopup();
+let initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
-  formElement.addEventListener('submit', formSubmitEditProfile);
-} 
-editButton.addEventListener('click', createPopupEditButton);
+]; 
 
-// Создание формы popup !--- Новое место ---!
-const createPopupNewPlace = () => {
+// создание template элемента edit-button 
+const createPopupEditButton = () => {
+  popupContainer.append(templateEditButton.content);
   popup.classList.add('popup_opend');
-  popupContainer.querySelector('.popup__title').textContent = 'Новое место'; 
-  let idNamePlace = popupContainer.querySelector('#person').id = 'name-place';
-  let idLinkImage = popupContainer.querySelector('#about-me').id = 'link-image';
-  idNamePlace = popupContainer.querySelector('#name-place');
-  idLinkImage = popupContainer.querySelector('#link-image');
-  idNamePlace.name = 'name-place';
-  idLinkImage.name = 'link-image';
-  idNamePlace.value = ' '
-  idLinkImage.value = ' '
-  idNamePlace.placeholder = 'Название'
-  idLinkImage.placeholder = 'Ссылка на картинку'
-
-  // Отправка формы для popup Новое место
+  // Отправка формы edit-button
   const formSubmitEditProfile  = (evt) => {
     evt.preventDefault(); 
+    personProfile.textContent = personInput.value;
+    aboutMeProfile.textContent = aboutMeInput.value;
     closePopup();
+    popup.removeChild(templateEditButton);
   }
-  formElement.addEventListener('submit', formSubmitEditProfile);
-} 
-addButton.addEventListener('click', createPopupNewPlace);
+  popupForm.addEventListener('submit', formSubmitEditProfile);
+}
+editButton.addEventListener('click', createPopupEditButton);
 
+// создание template элемента new-place
+const createPopupNewPlace = () => {
+  popupContainer.append(templateNewPlace.content)
+  popup.classList.add('popup_opend');
+  // Отправка формы new-place
+  const formSubmitNewPlace  = (evt) => {
+    evt.preventDefault(); 
+    closePopup();
+    popup.removeChild(templateNewPlace);
+  }
+  popupForm.addEventListener('submit', formSubmitNewPlace);
+}
+addButton.addEventListener('click', createPopupNewPlace); 
 
 // Закрытие popup 
-const closePopup = () => { popup.classList.remove('popup_opend')}
+const closePopup = () => { 
+  popup.classList.remove('popup_opend')
+}
 closeButton.addEventListener('click', closePopup);
-
-
-
-
-
 
 
 
 
 // 1. Шесть карточек «из коробки»
 
-// Я полностью убрал src в html, так как это лишняя загрузка
-// и если мы захотим поменять базовые картинки,
-// нам придется заменять их и в html и в массиве.
 
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ]; 
-
+// Загрузка place в массив
 const loadInitialCards = () => {
     const cards = document.querySelectorAll('.element') 
     const linkCards = document.querySelectorAll('.element__image')
@@ -118,8 +102,6 @@ loadInitialCards();
 
 
 
-
-
 // 4. Лайк карточки
 const likes = document.querySelectorAll('.element__like');
 
@@ -128,3 +110,17 @@ likes.forEach.call(likes, function(evt){
         evt.target.classList.toggle('element__like_active');
     })
 })
+
+// Открытие popup'a с картинкой
+
+const images = document.querySelectorAll('.element__image');
+let popupImage = document.querySelector('#zoom-image').content
+
+images.forEach.call(images, function(evt){
+  evt.addEventListener('click', function(evt){
+    document.body.append(popupImage) // нужно создать попап и
+    // добавлять по ноебходимости
+  })
+})
+
+// evt.target.src
