ea56a4a4f092a5e6694c8cf74d33a8d381f7dada
Maxim <kejerino@yandex.ru>
2022-01-30 00:42:05 +0800

    Creating functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,89 @@
-const popup = document.querySelectorAll('.popup')
-const popupEditButton = document.querySelector('#popup-edit-button')
-const popupAddButton = document.querySelector('#popup-add-button')
-const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close-button')
-const submitEditButton = document.querySelector('#submit-edit-button');
-const submitAddButton = document.querySelector('#submit-add-button');
+const popup = document.querySelector('.popup');
 
-const addButton = document.querySelector('.profile__add-button')
+const popupAddPlaceButton = document.querySelector('.popup_type_add-place')
+const popupEditProfileButton = document.querySelector('.popup_type_edit-profile')
+
+
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const closeButton = document.querySelector('.popup__close-button')
+const submitEditButton = document.querySelector('#edit');
+const elements = document.querySelector('.elements')
+const templateElement = document.querySelector('#element-template').content
 
 let personInput = document.querySelector('#person')
 let aboutMeInput = document.querySelector('#about-me')
 let aboutMeProfile = document.querySelector('.profile__about-me')
 let personProfile = document.querySelector('.profile__person')
+let initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
+//Активация лайка
+const likeActive = (evt) => {
+    evt.target.classList.toggle('element__like_active');
+}
+
+//Удаление карточки
+const trashActive = (evt) => {
+    evt.target.parentElement.parentElement.remove();
+}
+
+
+// Темплейт форма
+const loadingElements = () => {
+    for (let i = 0; i < initialCards.length; i++) {
+        const element = templateElement.querySelector('.element').cloneNode(true)
+        const elementTitle = element.querySelector('.element__title');
+        const elementImage = element.querySelector('.element__image');
+        const elementLike = element.querySelector('.element__like')
+        const elementTrash = element.querySelector('.element__trash')
+
+        elementTitle.textContent = initialCards[i].name;
+        elementImage.src = initialCards[i].link;
+        elementLike.addEventListener('click', likeActive);
+        elementTrash.addEventListener('click', trashActive);
+
+        elements.append(element)
+    }
+}
+loadingElements();
+
+
+// Создание popup добавить место
+
+createPopupAddButton = () => {
+    popupAddPlaceButton.classList.add('popup_opened');
+}
+
 
 // Создание popup Редактировать профиль
 const createPopupEditButton = () => {
-    popupEditButton.classList.add('popup_opend');
+    popupEditProfileButton.classList.add('popup_opened');
     personInput.value = personProfile.textContent;
     aboutMeInput.value= aboutMeProfile.textContent;
 }
@@ -28,25 +96,18 @@ const handlerFormEditButton = (evt) => {
     closePopup();
 }
 
-//Создание popup Добавить место
-const createPopupAddButton = () => {
-    popupAddButton.classList.add('popup_opend');
-}
-
-//Отправка формы Добавить место
-const handlerFormAddButton = (evt) => {
-    evt.preventDefault();
-}
-
-
 //Закрыть попап
 const closePopup = () => {
-    popup.classList.remove('popup_opend');
+    // Придумать как закрыть 2 попапа!!!!!
 }
+
+
+
+
+// Добавление карточки сделать через копию темплейта и присвоения тайтла и картинки !!!
 
 
 addButton.addEventListener('click', createPopupAddButton)
 editButton.addEventListener('click', createPopupEditButton);
 closeButton.addEventListener('click', closePopup);
-submitEditButton.addEventListener('submit', handlerFormEditButton);
-submitAddButton.addEventListener('submit', handlerFormEditButton);
+// submitEditButton.addEventListener('submit', handlerFormEditButton);
