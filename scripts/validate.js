c0a100386d79b9c9bacf95226aae140593581c82
Maxim <kejerino@yandex.ru>
2022-02-14 22:54:52 +0800

    comments

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,6 +3,7 @@ const enableValidation = data => {
     forms.forEach(form => addFormsListener(form, data))
 }
 
+// Добавление форм
 const addFormsListener = (form, config) => {
     form.addEventListener('submit', handleSubmit)
     form.addEventListener('input', () => setSubmitButtonState(form, config))
@@ -13,11 +14,12 @@ const addFormsListener = (form, config) => {
     setSubmitButtonState(form, config);
 }
 
-
+//Сбор отправки формы
 const handleSubmit = evt => {
     evt.preventDefault()
 }
 
+//Проверка на волидность формы
 const handleField = (form, input, config) => {
     if (input.validity.valid) {
         hideErrors(form, input,config)
@@ -26,12 +28,14 @@ const handleField = (form, input, config) => {
     }
 }
 
+//Показать ошибки
 const showErrors = (form, input, config) => {
     input.classList.add(config.inputErrorClass);
     const errorElement = form.querySelector(`#${input.id}-errors`);
     checkingInputErrors(input, errorElement);
 }
 
+// Проверка инпутов на ошибки
 const checkingInputErrors = (input, error) => {
     if (input.type === 'text' && input.value.length === 1){
         error.textContent = `Минимальное количество символов: 2. Длина текста сейчас: ${input.value.length} символ.`
@@ -44,19 +48,19 @@ const checkingInputErrors = (input, error) => {
     }
 }
 
+//Убрать ошибки
 const hideErrors = (form, input, config) => {
     input.classList.remove(config.inputErrorClass);
     const errorElement = form.querySelector(`#${input.id}-errors`);
     errorElement.textContent = ''
 }
 
-
+// Проверка состояния кнопки сабмит
 const setSubmitButtonState = (form, config) => {
     const button = form.querySelector(config.submitButtonSelector)
     button.disabled = !form.checkValidity();
     button.classList.toggle(config.inactiveButtonClass, !form.checkValidity())
 }
-
 
 enableValidation(formValidationConfig);
 
